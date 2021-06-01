f93ad6157ba4f064869c83ddb810ea9de064953a
Tohir <toxa.tb9@gmail.com>
2021-06-01 18:14:42 +0600

    fix home

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-let swiper = new Swiper(".mySwiper", {
+let swiper_01 = new Swiper(".swiper-01", {
   pagination: {
     el: ".swiper-pagination",
   },
 });
+let swiper_02 = new Swiper(".swiper-02", {});
+let swiper_03 = new Swiper(".swiper-03", {});
+let swiper_04 = new Swiper(".swiper-04", {});
+let swiper_05 = new Swiper(".swiper-05", {});
+
+// let i = 0;
+// window.addEventListener("scroll", navbarScroll);
+// navbarScroll();
+// function navbarScroll() {
+//   i++;
+//   console.log(i);
+// }
